5fab696a6c1bb63b727973a2fe42f5320cdc3bde
sawad@cairo.aas.com.sa <SAwad@cairo.aas.com.sa>
2024-09-05 15:01:44 +0300

    modify wishlist locators

--- src/test/java/org/example/pages/P08_WishlistLocators.java
+++ src/test/java/org/example/pages/P08_WishlistLocators.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.By;
 public class P08_WishlistLocators {
     public By wishlistButtonHTC = By.cssSelector("div[data-productid='18'] button.add-to-wishlist-button");
     public By successMessage = By.cssSelector("div.bar-notification.success");
-    public By wishlistTab = By.cssSelector("a[href='/wishlist']");
+    public By wishlistTab = By.cssSelector("a.ico-wishlist");
     public By wishlistQty = By.cssSelector("input.qty-input");
 }
 
